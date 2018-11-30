2986940fb9ee721539a91f7af95a8775bf20f3d6
CristianMoralesLopez <cristianalejandromoraleslopez@gmail.com>
2018-11-30 04:19:00 -0500

    homesinregistro actualizado

--- app/src/main/java/co/potes/icesi/startagrocol/model/AdaptadorListaHomeSinRegistrar.java
+++ app/src/main/java/co/potes/icesi/startagrocol/model/AdaptadorListaHomeSinRegistrar.java
@@ -13,7 +13,9 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import co.potes.icesi.startagrocol.R;
 
@@ -49,18 +51,28 @@ public class AdaptadorListaHomeSinRegistrar extends BaseAdapter {
         View renglon = inflater.inflate(R.layout.xmlisthome, null, false);
         TextView txtTiulto = renglon.findViewById(R.id.lista);
         TextView txtDescripcion = renglon.findViewById(R.id.usuario);
+        TextView txtFecha= renglon.findViewById(R.id.fechaProyecto);
+        TextView monto = renglon.findViewById(R.id.valorProyecto);
+
 
 
         ImageView image = renglon.findViewById(R.id.iv_foto);
 
         txtTiulto.setText( proyectos.get(position).getTitulo());
-        txtDescripcion.setText(proyectos.get(position).getDescripcion());
+        txtDescripcion.setText("Descripcion"+"\n"+"\n"+proyectos.get(position).getDescripcion());
+        txtFecha.setText("Fecha de Cierre: "+proyectos.get(position).getFechaCierreProyecto());
+
+        int valor = Integer.parseInt(proyectos.get(position).getValorProyecto());
+
+        NumberFormat format =  NumberFormat.getCurrencyInstance(new Locale("es","CO"));
+
+
+        monto.setText("valor del proyecto: "+ format.format(valor));
 
 
 
 
         Picasso.get().load(proyectos.get(position).getImagenPrimaria()).into(image);
-
 
 
         return renglon;
